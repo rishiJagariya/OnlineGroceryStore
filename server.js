93d4ea1b3aa64a6574cbfe6896d9e6394c16feed
Rishi jagariya <43576913+rishiJagariya@users.noreply.github.com>
2020-06-11 19:50:13 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,71 @@
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const express = require('express')
+const app = express()
+const bcrypt = require('bcrypt')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+
+const initializePassport = require('./passport-config')
+initializePassport(
+    passport,
+    email => users.find(user => user.email === email),
+    id => users.find(user => user.id === id)
+)
+
+const users = []
+
+app.use(express.static(__dirname + '/views'));
+app.use(express.urlencoded({ extended: false}))
+app.use(flash())
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.set('view-engine', 'ejs')
+
+app.get('/',(req,res) => {
+    res.render('index.ejs', { name: rishi})
+})
+
+app.get('/login',(req,res) => {
+    res.render('login.ejs')
+})
+
+app.post('/login',passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+app.get('/signup',(req,res) => {
+    res.render('signup.ejs')
+})
+
+app.post('/signup', async (req,res) => {
+    try {
+        const hasedPassword = await bcrypt.hash(req.body.password, 10)
+        users.push({
+            id: Date.now().toString(),
+            name: req.body.name,
+            email: req.body.email,
+            password: hasedPassword
+        })
+        res.redirect('/login')
+    } catch {
+        res.redirect('/signup')    
+    }
+    console.log(users)
+})
+
+app.listen(3000,()=>
+{
+    console.log("cfghfxgxg")
+})
